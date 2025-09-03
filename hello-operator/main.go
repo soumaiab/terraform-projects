e5657b3299c9134072ba695efa15da401796934d
soumaiab <soumaia.bouhouia@mail.concordia.ca>
2025-09-03 19:22:34 -0400

    [~] Updated the code so that there are 2 watchers: one for secret changes and one for config changes and put them in a queue. If the secret is changed, the operator automatically rolls out a new Pod since we track secret changes with a checksum

--- hello-operator/main.go
+++ hello-operator/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"os"
@@ -19,8 +21,10 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+// A channel acts as our work queue (holds HelloApp names to reconcile)
+var workqueue = make(chan string, 100)
+
 func main() {
-	// Load kubeconfig (from ~/.kube/config by default)
 	kubeconfig := flag.String("kubeconfig", os.Getenv("HOME")+"/.kube/config", "path to kubeconfig file")
 	flag.Parse()
 
@@ -29,163 +33,225 @@ func main() {
 		panic(err)
 	}
 
-	// Dynamic client (works with CRDs)
 	dynClient, err := dynamic.NewForConfig(config)
 	if err != nil {
 		panic(err)
 	}
-
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err)
 	}
 
-	// Tell client which resource we want
 	helloAppRes := schema.GroupVersionResource{
 		Group:    "apps.example.com",
 		Version:  "v1alpha1",
-		Resource: "helloapps", // plural name from the CRD
+		Resource: "helloapps",
 	}
 
-	// Watch for HelloApp objects
-	watcher, err := dynClient.Resource(helloAppRes).Namespace("default").Watch(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Watching HelloApp resources in 'default' namespace...")
-
-	// Handle signals so we can stop cleanly
-	stopCh := make(chan os.Signal, 1)
-	signal.Notify(stopCh, syscall.SIGINT, syscall.SIGTERM)
-
+	// --- Worker: processes items from the queue one by one ---
 	go func() {
-		for event := range watcher.ResultChan() {
-			obj := event.Object.(*unstructured.Unstructured)
-			name := obj.GetName()
-			spec := obj.Object["spec"].(map[string]interface{})
-
-			// pull replicas (default 1 if nil)
-			replicas := int32(1)
-			if r, ok := spec["replicas"].(int64); ok {
-				replicas = int32(r)
-			}
-
-			// pull image
-			image := "hellogo:latest"
-			if i, ok := spec["image"].(string); ok {
-				image = i
-			}
-
-			secretName := ""
-			if s, ok := spec["secretName"].(string); ok {
-				secretName = s
-			}
-
-			fmt.Printf("Reconciling HelloApp %s → replicas=%d, image=%s\n", name, replicas, image)
-
-			// desired Deployment
-			deploy := &appsv1.Deployment{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      name,
-					Namespace: "default",
-				},
-				Spec: appsv1.DeploymentSpec{
-					Replicas: &replicas,
-					Selector: &metav1.LabelSelector{
-						MatchLabels: map[string]string{"app": name},
-					},
-					Template: corev1.PodTemplateSpec{
-						ObjectMeta: metav1.ObjectMeta{
-							Labels: map[string]string{"app": name},
-						},
-						Spec: corev1.PodSpec{
-							Containers: []corev1.Container{{
-								Name:  "server",
-								Image: image,
-								Ports: []corev1.ContainerPort{{
-									ContainerPort: 8080,
-								}},
-								Env: []corev1.EnvVar{
-									{
-										Name: "APP_MESSAGE",
-										ValueFrom: &corev1.EnvVarSource{
-											SecretKeyRef: &corev1.SecretKeySelector{
-												LocalObjectReference: corev1.LocalObjectReference{
-													Name: secretName,
-												},
-												Key: "APP_MESSAGE",
-											},
-										},
-									},
-								},
-							}},
-						},
-					},
-				},
-			}
-
-			// Check if Deployment exists
-			existing, err := clientset.AppsV1().Deployments("default").Get(context.TODO(), name, metav1.GetOptions{})
-			if err == nil {
-				// update
-				existing.Spec = deploy.Spec
-				_, err = clientset.AppsV1().Deployments("default").Update(context.TODO(), existing, metav1.UpdateOptions{})
-				if err != nil {
-					fmt.Println("Error updating Deployment:", err)
-				} else {
-					fmt.Println("Updated Deployment", name)
-				}
-			} else {
-				// create
-				_, err = clientset.AppsV1().Deployments("default").Create(context.TODO(), deploy, metav1.CreateOptions{})
-				if err != nil {
-					fmt.Println("Error creating Deployment:", err)
-				} else {
-					fmt.Println("Created Deployment", name)
-				}
-			}
-
-			// Desired Service
-			svc := &corev1.Service{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      name,
-					Namespace: "default",
-				},
-				Spec: corev1.ServiceSpec{
-					Selector: map[string]string{"app": name},
-					Ports: []corev1.ServicePort{{
-						Protocol:   corev1.ProtocolTCP,
-						Port:       80,
-						TargetPort: intstr.FromInt(8080),
-					}},
-				},
-			}
-
-			// Check if Service exists
-			existingSvc, err := clientset.CoreV1().Services("default").Get(context.TODO(), name, metav1.GetOptions{})
-			if err == nil {
-				// Service exists → update spec (only selector + ports matter)
-				existingSvc.Spec = svc.Spec
-				_, err = clientset.CoreV1().Services("default").Update(context.TODO(), existingSvc, metav1.UpdateOptions{})
-				if err != nil {
-					fmt.Println("Error updating Service:", err)
-				} else {
-					fmt.Println("Updated Service", name)
-				}
-			} else {
-				// Create Service
-				_, err = clientset.CoreV1().Services("default").Create(context.TODO(), svc, metav1.CreateOptions{})
-				if err != nil {
-					fmt.Println("Error creating Service:", err)
-				} else {
-					fmt.Println("Created Service", name)
-				}
-			}
-
+		for name := range workqueue {
+			reconcileByName(name, dynClient, clientset, helloAppRes)
 		}
 	}()
 
+	// --- HelloApp watcher ---
+	helloWatcher, err := dynClient.Resource(helloAppRes).Namespace("default").Watch(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	go func() {
+		for event := range helloWatcher.ResultChan() {
+			obj := event.Object.(*unstructured.Unstructured)
+			workqueue <- obj.GetName()
+		}
+	}()
+
+	// --- Secret watcher ---
+	secretWatcher, err := clientset.CoreV1().Secrets("default").Watch(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	go func() {
+		for event := range secretWatcher.ResultChan() {
+			secret := event.Object.(*corev1.Secret)
+			secretName := secret.GetName()
+
+			// Find HelloApps that reference this secret
+			list, err := dynClient.Resource(helloAppRes).Namespace("default").List(context.TODO(), metav1.ListOptions{})
+			if err != nil {
+				fmt.Println("Error listing HelloApps:", err)
+				continue
+			}
+			for _, item := range list.Items {
+				spec := item.Object["spec"].(map[string]interface{})
+				if s, ok := spec["secretName"].(string); ok && s == secretName {
+					fmt.Printf("Secret %q changed → enqueue HelloApp %q\n", secretName, item.GetName())
+					workqueue <- item.GetName()
+				}
+			}
+		}
+	}()
+
+	fmt.Println("Watching HelloApp and Secret resources in 'default' namespace...")
+
+	// --- Stop handling ---
+	stopCh := make(chan os.Signal, 1)
+	signal.Notify(stopCh, syscall.SIGINT, syscall.SIGTERM)
 	<-stopCh
 	fmt.Println("Shutting down...")
+}
+
+// Pull the HelloApp again from API before reconciling
+func reconcileByName(name string, dynClient dynamic.Interface, clientset *kubernetes.Clientset, helloAppRes schema.GroupVersionResource) {
+	obj, err := dynClient.Resource(helloAppRes).Namespace("default").Get(context.TODO(), name, metav1.GetOptions{})
+	if err != nil {
+		fmt.Printf("Error fetching HelloApp %q: %v\n", name, err)
+		return
+	}
+	reconcileHelloApp(obj, clientset)
+}
+
+// Actual reconcile logic
+func reconcileHelloApp(obj *unstructured.Unstructured, clientset *kubernetes.Clientset) {
+	name := obj.GetName()
+	spec := obj.Object["spec"].(map[string]interface{})
+
+	// replicas
+	replicas := int32(1)
+	if r, ok := spec["replicas"].(int64); ok {
+		replicas = int32(r)
+	}
+
+	// image
+	image := "hellogo:latest"
+	if i, ok := spec["image"].(string); ok {
+		image = i
+	}
+
+	// secretName
+	secretName := ""
+	if s, ok := spec["secretName"].(string); ok {
+		secretName = s
+	}
+
+	// fetch the Secret
+	secret, err := clientset.CoreV1().Secrets("default").Get(context.TODO(), secretName, metav1.GetOptions{})
+	if err != nil {
+		fmt.Println("Error fetching Secret:", err)
+		return
+	}
+
+	// compute checksum
+	checksum := hashSecretData(secret.Data)
+
+	fmt.Printf("Reconciling HelloApp %q → replicas=%d, image=%s, secret=%s\n", name, replicas, image, secretName)
+
+	// Desired Deployment
+	deploy := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: "default",
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"app": name},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{"app": name},
+					Annotations: map[string]string{
+						"secret-checksum": checksum,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Name:  "server",
+						Image: image,
+						Ports: []corev1.ContainerPort{{
+							ContainerPort: 8080,
+						}},
+						Env: []corev1.EnvVar{{
+							Name: "APP_MESSAGE",
+							ValueFrom: &corev1.EnvVarSource{
+								SecretKeyRef: &corev1.SecretKeySelector{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: secretName,
+									},
+									Key: "APP_MESSAGE",
+								},
+							},
+						}},
+					}},
+				},
+			},
+		},
+	}
+
+	// Get latest Deployment before update
+	current, err := clientset.AppsV1().Deployments("default").Get(context.TODO(), name, metav1.GetOptions{})
+	if err == nil {
+		oldChecksum := current.Spec.Template.Annotations["secret-checksum"]
+		if oldChecksum != checksum {
+			fmt.Printf("Secret %q changed for HelloApp %q → old checksum=%s, new checksum=%s. Triggering rollout.\n",
+				secretName, name, oldChecksum, checksum)
+		}
+		current.Spec = deploy.Spec
+		_, err = clientset.AppsV1().Deployments("default").Update(context.TODO(), current, metav1.UpdateOptions{})
+		if err != nil {
+			fmt.Println("Error updating Deployment:", err)
+		} else {
+			fmt.Println("Updated Deployment", name)
+		}
+	} else {
+		_, err = clientset.AppsV1().Deployments("default").Create(context.TODO(), deploy, metav1.CreateOptions{})
+		if err != nil {
+			fmt.Println("Error creating Deployment:", err)
+		} else {
+			fmt.Println("Created Deployment", name)
+		}
+	}
+
+	// Ensure Service exists
+	svc := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: "default",
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{"app": name},
+			Ports: []corev1.ServicePort{{
+				Protocol:   corev1.ProtocolTCP,
+				Port:       80,
+				TargetPort: intstr.FromInt(8080),
+			}},
+		},
+	}
+	_, err = clientset.CoreV1().Services("default").Get(context.TODO(), name, metav1.GetOptions{})
+	if err == nil {
+		_, err = clientset.CoreV1().Services("default").Update(context.TODO(), svc, metav1.UpdateOptions{})
+		if err != nil {
+			fmt.Println("Error updating Service:", err)
+		} else {
+			fmt.Println("Updated Service", name)
+		}
+	} else {
+		_, err = clientset.CoreV1().Services("default").Create(context.TODO(), svc, metav1.CreateOptions{})
+		if err != nil {
+			fmt.Println("Error creating Service:", err)
+		} else {
+			fmt.Println("Created Service", name)
+		}
+	}
+}
+
+// helper: hash secret contents
+func hashSecretData(data map[string][]byte) string {
+	h := sha256.New()
+	for k, v := range data {
+		h.Write([]byte(k))
+		h.Write(v)
+	}
+	return hex.EncodeToString(h.Sum(nil))
 }
